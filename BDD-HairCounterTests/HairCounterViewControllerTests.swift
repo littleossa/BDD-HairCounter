4f2d8bd2e45587927d4b4bd7aaa130f77f74df2b
hiraoka.osamu <hiraoka.osamu@classmethod.jp>
2021-08-17 16:20:07 +0900

    [refactor] HairCounterViewControllerTestsでviewDidLoadまでの処理を繰り返ししていた為、リファクト

--- BDD-HairCounterTests/HairCounterViewControllerTests.swift
+++ BDD-HairCounterTests/HairCounterViewControllerTests.swift
@@ -9,36 +9,25 @@ import XCTest
 @testable import BDD_HairCounter
 
 class HairCounterViewControllerTests: XCTestCase {
+    
+    private var viewController: HairCounterViewController!
 
     override func setUpWithError() throws {
-    }
-
-    override func tearDownWithError() throws {
-    }
-    
-    func test_初期起動時_画像にhair0が表示されていること() {
         let stroyboard = UIStoryboard(name: "HairCounter", bundle: nil)
-        let viewController = stroyboard.instantiateInitialViewController() as! HairCounterViewController
+        viewController = stroyboard.instantiateInitialViewController() as? HairCounterViewController
         let window = UIWindow(frame: UIScreen.main.bounds)
         window.rootViewController = viewController
         // 画面表示を行う
         window.makeKeyAndVisible()
         // 初期起動時の処理を実行する
         viewController.viewDidLoad()
+    }
+    
+    func test_初期起動時_画像にhair0が表示されていること() {
         XCTAssertEqual(viewController.hairImageView.image, UIImage(named: "hair0"))
     }
     
-//    初期表示時に文字入力のルール説明の文字が表示されていること
     func test_初期表示時_文字入力ルール説明の文字が表示されていること() {
-        let stroyboard = UIStoryboard(name: "HairCounter", bundle: nil)
-        let viewController = stroyboard.instantiateInitialViewController() as! HairCounterViewController
-        let window = UIWindow(frame: UIScreen.main.bounds)
-        window.rootViewController = viewController
-        window.makeKeyAndVisible()
-        viewController.viewDidLoad()
         XCTAssertEqual(viewController.ruleDescriptionLabel.text, "0~10までの数字を入力してください")
     }
-    
-    
-//    初期表示時に０という文字が入力されていること
 }
