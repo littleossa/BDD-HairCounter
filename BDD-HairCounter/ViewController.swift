6e72fdf6024f3c15ed39da0a1aff1939d0446944
hiraoka.osamu <hiraoka.osamu@classmethod.jp>
2021-08-17 11:41:17 +0900

    Initial Commit

---
+++ BDD-HairCounter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BDD-HairCounter
+//
+//  Created by Osamu Hiraoka on 2021/08/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
