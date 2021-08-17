d73266a617b58ead3b25b0c785e87750e54b23c1
hiraoka.osamu <hiraoka.osamu@classmethod.jp>
2021-08-17 13:56:18 +0900

    [fix] UnitTestsが初期設定の名前だった為、HairCounterViewControllerTestsに変更

--- BDD-HairCounterTests/HairCounterViewControllerTests.swift
+++ BDD-HairCounterTests/HairCounterViewControllerTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import BDD_HairCounter
 
-class BDD_HairCounterTests: XCTestCase {
+class HairCounterViewControllerTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
