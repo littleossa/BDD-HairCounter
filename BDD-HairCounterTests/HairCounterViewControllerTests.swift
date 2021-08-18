e68ff13ab625cef41658542c9dc23af25b459e6d
hiraoka.osamu <hiraoka.osamu@classmethod.jp>
2021-08-19 08:16:23 +0900

    [refactor] リファクタリングの為、メンバー変数のhairImageViewとruleDescriptionを追加し、各コードに適用

--- BDD-HairCounterTests/HairCounterViewControllerTests.swift
+++ BDD-HairCounterTests/HairCounterViewControllerTests.swift
@@ -16,6 +16,14 @@ class HairCounterViewControllerTests: XCTestCase {
         return viewController.hairCountTextField
     }
 
+    private var hairImageView: UIImageView {
+        return viewController.hairImageView
+    }
+    
+    private var ruleDescriptionLabel: UILabel {
+        return viewController.ruleDescriptionLabel
+    }
+    
     override func setUpWithError() throws {
         let stroyboard = UIStoryboard(name: "HairCounter", bundle: nil)
         viewController = stroyboard.instantiateInitialViewController() as? HairCounterViewController
@@ -28,11 +36,11 @@ class HairCounterViewControllerTests: XCTestCase {
     }
     
     func test_初期起動時_画像にhair0が表示されていること() {
-        XCTAssertEqual(viewController.hairImageView.image, UIImage(named: "hair0"))
+        XCTAssertEqual(hairImageView.image, UIImage(named: "hair0"))
     }
     
     func test_初期表示時_文字入力ルール説明の文字が表示されていること() {
-        XCTAssertEqual(viewController.ruleDescriptionLabel.text, "0~10までの数字を入力してください")
+        XCTAssertEqual(ruleDescriptionLabel.text, "0~10までの数字を入力してください")
     }
     
     func test_初期起動時_テキストフィールドに0という文字が入力されていること() {
@@ -50,23 +58,23 @@ class HairCounterViewControllerTests: XCTestCase {
     func test_TextFieldの文字の値が変わった時_値が0から10_画像にその値に紐づく画像が表示されていること() {
         for i in 0...10 {
             hairCountTextField.valueChange(into: "\(i)", on: viewController)
-            XCTAssertEqual(viewController.hairImageView.image, UIImage(named: "hair\(i)"))
+            XCTAssertEqual(hairImageView.image, UIImage(named: "hair\(i)"))
         }
     }
     
     func test_TextFieldの文字の値が変わった時_値が11_画像にerrorが表示されていること() {
         hairCountTextField.valueChange(into: "11", on: viewController)
-        XCTAssertEqual(viewController.hairImageView.image, UIImage(named: "error"))
+        XCTAssertEqual(hairImageView.image, UIImage(named: "error"))
     }
     
     func test_TextFieldの文字の値が変わった時_値が12_画像にerrorが表示されていること() {
         hairCountTextField.valueChange(into: "12", on: viewController)
-        XCTAssertEqual(viewController.hairImageView.image, UIImage(named: "error"))
+        XCTAssertEqual(hairImageView.image, UIImage(named: "error"))
     }
     
     func test_TextFieldの文字の値が変わった時_値が何もない_画像にerrorが表示されていること() {
         hairCountTextField.valueChange(into: "", on: viewController)
-        XCTAssertEqual(viewController.hairImageView.image, UIImage(named: "error"))
+        XCTAssertEqual(hairImageView.image, UIImage(named: "error"))
     }
 }
 
